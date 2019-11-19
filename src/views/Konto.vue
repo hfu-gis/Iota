acd2628a5301ad69f105c999e3e0f674a85ff045
MorizzF <56886540+MorizzF@users.noreply.github.com>
2019-11-19 16:21:33 +0100

    Merge branch 'master' of https://github.com/hfu-gis/demo # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ src/views/Konto.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="body-1">
+    <body>
+    <a href="Bilanz%20anzeigen.html">Bilanz anzeigen</a>
+    <a href="Sparen.html">Sparen</>
+    <a href="Kontostand.html">Kontostand</a>
+    <
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  // gebt jeder Page einen eigenen Namen
+  name: 'template',
+
+  // benötigte Komponenten
+  components: {},
+
+  // entspricht den HTML-Attributen
+  props: {},
+
+  // Variablen-Speicher
+  data() {
+    return {}
+  },
+
+  // reagieren auf prop-Veränderung
+  watch: {},
+
+  // interne Methoden
+  methods: [],
+
+  // Initialisierung
+  created() {}
+}
+</script>
+
+<style scoped>
+/* CSS für diese Seite hier einfügen */
+</style>
---
+++ src/views/Kontostad.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="body-1">
+    <body>
+
+    <p> Ihr Kontostand beträgt: </p>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  // gebt jeder Page einen eigenen Namen
+  name: 'template',
+
+  // benötigte Komponenten
+  components: {},
+
+  // entspricht den HTML-Attributen
+  props: {},
+
+  // Variablen-Speicher
+  data() {
+    return {}
+  },
+
+  // reagieren auf prop-Veränderung
+  watch: {},
+
+  // interne Methoden
+  methods: [],
+
+  // Initialisierung
+  created() {}
+}
+</script>
+
+<style scoped>
+/* CSS für diese Seite hier einfügen */
+</style>
