7b20b7e9db0763da9d03613696fae2580f56ef6f
konradmi-1807 <michelle.konrad@hs-furtwangen.de>
2019-12-18 12:30:49 +0100

    Router hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
               clipped
       >
         <v-list dense>
-          <v-list-item link>
+
+          <v-list-item link :to="{name:'Dashboard'}">
             <v-list-item-action>
               <v-icon>mdi-view-dashboard</v-icon>
             </v-list-item-action>
@@ -16,7 +17,8 @@
               <v-list-item-title>Dashboard</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-          <v-list-item link>
+
+          <v-list-item link :to="{name:'Bezahlen'}">
             <v-list-item-action>
               <v-icon>mdi-settings</v-icon>
             </v-list-item-action>
@@ -48,49 +50,12 @@
               clipped-left
       >
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-toolbar-title>Finanz App</v-toolbar-title>
       </v-app-bar>
 
-      <v-content>
-        <v-container
-                class="fill-height"
-                fluid
-        >
-          <v-row
-                  align="center"
-                  justify="center"
-          >
-            <v-col class="shrink">
-              <v-tooltip right>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                          :href="source"
-                          icon
-                          large
-                          target="_blank"
-                          v-on="on"
-                  >
-                    <v-icon large>mdi-code-tags</v-icon>
-                  </v-btn>
-                </template>
-                <span>Source</span>
-              </v-tooltip>
-              <v-tooltip right>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                          icon
-                          large
-                          href="https://codepen.io/johnjleider/pen/bXNzZL"
-                          target="_blank"
-                          v-on="on"
-                  >
-                    <v-icon large>mdi-codepen</v-icon>
-                  </v-btn>
-                </template>
-                <span>Codepen</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
+      <v-content light>
+        <v-container>
+          <router-view></router-view>
         </v-container>
       </v-content>
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import Login from '../views/Login'
 import HelloWorld from "../components/HelloWorld";
 import AppLogin from "../views/AppLogin";
 import Socialmedia from "../views/Social media Login";
-import Register from "../views/Register";
+import Dashboard from "../views/Dashboard";
 import Konto from "../views/Konto";
 import Bezahlen from "../views/Bezahlen";
+
 Vue.use(VueRouter)
 
+//let Dashboard;
 export default new VueRouter({
     routes: [
         {
@@ -21,12 +23,15 @@ export default new VueRouter({
         },
         {
             path: '/Bezahlen',
+            name: 'Bezahlen',
             component: Bezahlen
         },
 
 
 
-        {   path: '/Register',
-            component: Register},
+        {   path: '/Dashboard',
+            name: 'Dashboard',
+            component: Dashboard
+        },
     ]
 })
